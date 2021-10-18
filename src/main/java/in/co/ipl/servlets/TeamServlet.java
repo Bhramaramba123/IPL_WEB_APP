b8564bbde7db09bbecf9602dda17cd06ff76bd77
bhramaramba_ds <bhramaramba_ds@THBSLAPTOP-728.thbs.india.com>
2021-10-18 20:50:50 +0530

    Code Updated(18/10/2021)

---
+++ src/main/java/in/co/ipl/servlets/TeamServlet.java
@@ -0,0 +1,42 @@
+package in.co.ipl.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TeamServlet
+ */
+public class TeamServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TeamServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		log(request.getParameter("name"));
+		request.setAttribute("team", "TeamDetails");
+		request.getRequestDispatcher("/WEB-INF/team.jsp").forward(request, response);;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/home.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Home JSP</title>
+</head>
+<body>
+<a href="/webapp/team?name=RCB"><h1>Rayal Chalengers Bangalore</h1></a>
+<a href="/webapp/team?name=CSK"><h1>Channai Super Kings</h1></a>
+
+</body>
+</html>
